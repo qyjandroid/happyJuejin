dc8216126b54e3e4faa68ffcd0176cc4ecf9e914
权跃杰 <quanyj@mail.jj.cn>
2023-10-13 10:40:57 +0800

    feat: 更新

--- src/tasks/autoMineCount.ts
+++ src/tasks/autoMineCount.ts
@@ -14,6 +14,7 @@ export default async function autoMineCount(browser: Browser, _page: Page, accou
     let page: Page;
     try {
         page = await browser.newPage();
+        page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36");
         await page.goto("https://juejin.cn/user/center/signin?avatar_menu", {
             waitUntil: "load"
         });
